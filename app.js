cbe9771c1b29e7ceb220339659523c6072c87d4b
salvador <salva@gmail,com>
2024-12-10 18:07:42 +0100

    Changes to be committed: 	new file:   app.js 	new file:   index.html 	new file:   node_modules/.bin/node-gyp-build 	new file:   node_modules/.bin/node-gyp-build-optional 	new file:   node_modules/.bin/node-gyp-build-optional.cmd 	new file:   node_modules/.bin/node-gyp-build-optional.ps1 	new file:   node_modules/.bin/node-gyp-build-test 	new file:   node_modules/.bin/node-gyp-build-test.cmd 	new file:   node_modules/.bin/node-gyp-build-test.ps1 	new file:   node_modules/.bin/node-gyp-build.cmd 	new file:   node_modules/.bin/node-gyp-build.ps1 	new file:   style.css

---
+++ app.js
@@ -0,0 +1,25 @@
+const { SerialPort, ReadlineParser  } = require('serialport')
+
+const port = new SerialPort({
+  path: 'COM5',
+  baudRate: 9600,
+  
+})
+
+
+port.open(function (err) {
+  if (err) {
+    return console.log('Error abriendo puerto: ', err.message)
+    port.close(console.log("puerto cerrado"))
+  }
+  
+})
+
+const parser = new ReadlineParser()
+port.pipe(parser)
+parser.on('data', console.log)
+
+
+
+
+
